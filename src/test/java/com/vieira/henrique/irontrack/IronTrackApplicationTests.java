6ecaca59d1c946ae03d2a6d30be40f9c7b026fc4
h-nriquevieira <h.nriquevieira@gmail.com>
2023-08-20 14:41:31 -0300

    Initial commit

---
+++ src/test/java/com/vieira/henrique/irontrack/IronTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.henrique.irontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
